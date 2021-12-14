c26f3b59fd9754801439a8f430e10b7f30f57a52
Mike Writer <napewnonieja123@wp.pl>
2021-12-14 16:27:45 +0100

    wladyslaw brek

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ client.on('message', msg => {
         const attachment3 = new MessageAttachment('https://cdn.discordapp.com/attachments/389514334943838210/920312845009305681/2x_2.gif');
         msg.channel.send(attachment3);
     } else if (msg.content.toLowerCase().startsWith ('powiedz mi fraszke')) {
-        msg.reply(msg.channel.send.this.author + "  ty chuju")
+        msg.reply('<@user>' + "  ty chuju")
 
     } else if (msg.content.includes ("kk")) {
         msg.reply("jestem z ciebie rozczarowany <:biblethump:798295427836936222>")
